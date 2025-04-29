1a63e39c2b2926c2a3fccb4188e208d0a491d88c
trusthemind <pluserwork24@gmail.com>
2025-04-29 18:58:31 +0300

    fixL settup cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express from "express";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
 import AuthRoutes from "./routes/auth";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(helmet());
 app.use(express.json());
 app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
